45d9fe5721cce16e3b0a98c97d23b6dff0d3a3ac
Jafer Haider <itsjafer@Jafers-MacBook-Pro.local>
2019-05-24 19:35:35 -0700

    Most features are now parsed through imdb, we just need to save it to a csv now

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -27,16 +27,19 @@ headers = \
 start_time = time()
 requests = 0
 tv_shows = list()
+tv_shows_with_features = list()
 
 # Get list of tv show names from csv
 with open("tv_shows.csv", 'r') as f:
     csv_reader = csv.reader(f, delimiter=',')
     tv_shows = list(csv_reader)
 
-for show in tv_shows:
-    # We want to query imdb one time
-    url = 'https://www.imdb.com/find?q=' + show[0] + '&s=tt&ttype=tv&exact=true'
+flat_tv_shows = [item for sublist in tv_shows for item in sublist]
 
+for show in flat_tv_shows:
+    print(show)
+    # We want to query imdb one time
+    url = 'https://www.imdb.com/search/title?title=' + show + '&title_type=tv_series'
     # Making a response and parsing it
     response = get(url, headers=headers)
 
@@ -55,7 +58,9 @@ for show in tv_shows:
 
     # We only care about the divs that have the movie name
     # imdb_page has the link to the tv show's imdb page
-    imdb_page = "https://www.imdb.com" + html_soup.find(class_='result_text').find('a').get('href')
+    if (len(html_soup.find_all(class_='lister-item-header')) <= 0):
+        continue
+    imdb_page = "https://www.imdb.com" + html_soup.find(class_='lister-item-header').find('a').get('href')
 
     # Now we want to do the same thing as before and get features
     # Some code duplication here unfortunately
@@ -74,15 +79,41 @@ for show in tv_shows:
     clear_output(wait = True)
 
     # Now we need to parse features
-    #   * Number of seasons
-    #   * User rating
-    #   * Genres
-    #   * Release date
-    #   * Episode length
 
-    num_seasons = html_soup.find(class_='seasons-and-year-nav').find('a').text
-    user_rating = html_soup.find(itemprop='ratingValue').text
-    print(html_soup.find_all(class_='see-more inline canwrap')[1].find_all('a'))
-    break
+    # Number of seasons
+    if (len(html_soup.find_all(class_='seasons-and-year-nav')) <= 0):
+        num_seasons = 0
+    else:
+        num_seasons = html_soup.find(class_='seasons-and-year-nav').find('a').text
+    
+    # User rating
+    if (len(html_soup.find(itemprop='ratingValue')) <= 0):
+        user_rating = 0
+    else:
+        user_rating = html_soup.find(itemprop='ratingValue').text
+
+    # Keywords
+    keywords = list()
+    for soup in html_soup.find_all(class_='see-more inline canwrap'):
+        for word in soup.find_all('a'):
+            if (word.text[0:7] == 'See All'):
+                continue
+            keywords.append(word.text.strip().lower())
+
+    # Episode length
+    if len(html_soup.find_all(id='titleDetails')) <= 0 or \
+    len(html_soup.find(id='titleDetails').find_all('time')) <= 0:
+        length = 0
+    else:
+        length = html_soup.find(id='titleDetails').find('time').text
+    
+    # Now we need to make a row
+    row = (show, num_seasons, user_rating, keywords, length)
+    tv_shows_with_features.append(row)
+
+    if requests > 15:
+        break
+
+print(tv_shows_with_features)
     
 
